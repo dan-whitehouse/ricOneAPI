473cf0bb8bbc4c057d8593c4eeb968a469589f71
daniel.whitehouse <daniel.whitehouse@D8Z7HX12.CRB.LOCAL>
2014-11-12 15:48:58 -0500

    Mapping Update

--- api/api/mapping/R1Mapper.java
+++ api/api/mapping/R1Mapper.java
@@ -1,6 +1,10 @@
 package api.mapping;
 
 import sif.dd.us32.model.K12SchoolType;
+import sif.dd.us32.model.K12StudentType.AcademicRecord;
+import sif.dd.us32.model.K12StudentType.Demographic;
+import sif.dd.us32.model.K12StudentType.Identity;
+import sif.dd.us32.model.K12StudentType.Identity.Name;
 import sif.dd.us32.model.ObjectFactory;
 import sif.dd.us32.model.K12StudentType;
 import sif.dd.us32.model.K12StaffType;
@@ -17,17 +21,56 @@ public class R1Mapper
 	public K12StudentType modelToSIF(R1Student r1Student)
 	{
 		K12StudentType sifStudent = oFac.createK12StudentType();
-		sifStudent.setRefId(r1Student.getStudentRefId());
+		Identity sifIdentity = oFac.createK12StudentTypeIdentity();
+		Demographic sifDemographic = oFac.createK12StudentTypeDemographic();
+		AcademicRecord sifAcademicRecord = oFac.createK12StudentTypeAcademicRecord();
 		
+		//Identity
+		Name name = new Name();
+		name.setFirstName(r1Student.getFirstName());
+		name.setMiddleName(r1Student.getMiddleName());
+		name.setLastName(r1Student.getLastName());
+		name.setPrefix(r1Student.getPrefix());
+		sifIdentity.setName(name);
+		
+		//Demographics
+		sifDemographic.setSex(r1Student.getSexCode());
+				
+		
+		//Academics
+		sifAcademicRecord.setCohortDescription(r1Student.getCohortGraduationYear());
+		
+		//Set K12Student
+		sifStudent.setRefId(r1Student.getStudentRefId());
+		sifStudent.setIdentity(sifIdentity);
+		sifStudent.setDemographic(sifDemographic);
+		sifStudent.setAcademicRecord(sifAcademicRecord);
 		return sifStudent;
 	}
 	
-	public R1Student sifToModel(K12StudentType sifStudent)
+	/*public R1Student sifToModel(K12StudentType sifStudent)
 	{
 		R1Student r1Student = new R1Student();
-		r1Student.setStudentRefId(sifStudent.getRefId());
+		r1Student.setStudentRefId(sifStudent.getRefId());		
+		
+		//Name
+		r1Student.setFirstName(sifStudent.getIdentity().getName().getFirstName());
+		r1Student.setMiddleName(sifStudent.getIdentity().getName().getMiddleName());
+		r1Student.setLastName(sifStudent.getIdentity().getName().getLastName());
+		r1Student.setPrefix(sifStudent.getIdentity().getName().getPrefix());
+				
+		//Demographic
+		r1Student.setSexCode(sifStudent.getDemographic().getSex());
+		//r1Student.setBirthDate(sifStudent.getDemographic().getBirthdate());
+		//r1Student.setHispanicLatinoEthnicity(sifStudent.getDemographic().getHispanicOrLatinoEthnicity()); //returns string..should it be ?
+		//r1Student.setUsCitizenshipStatusCode(sifStudent.getDemographic().???);	
+				
+		//r1Student.setCohortGraduationYear(sifStudent.getAcademicRecord().getCohortDescription());
+		//r1Student.setCounselor(sifStudent.getAcademicRecord().get);
+		//r1Student.setGenerationCode(sifStudent.getDemographic().);	
+		
 		return r1Student;
-	}
+	}*/
 	
 	//--Staff---------------------------------------------------------------//
 		public K12StaffType modelToSIF(R1Staff r1Staff)
